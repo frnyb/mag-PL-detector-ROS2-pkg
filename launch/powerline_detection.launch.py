a0c835893c340443678cc65bef4563de1f5e33bc
Frederik Falk Nyboe <frnyb16@student.sdu.dk>
2022-04-28 11:31:15 +0200

    Added os import

--- launch/powerline_detection.launch.py
+++ launch/powerline_detection.launch.py
@@ -5,6 +5,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
+import os
 
 def generate_launch_description():
     config = os.path.join(
